968974c4cb941ff2231c7e6645870ee679c0b4e0
Christian <chrisdaq@gmail.com>
2023-04-26 12:11:22 -0400

    styles and some content added

--- src/GifExpertApp.jsx
+++ src/GifExpertApp.jsx
@@ -1,7 +1,14 @@
+import { useState } from "react";
+
 export const GifExpertApp = () => {
+
+    const [categories, setCategories] = useState(['Ippo', 'DBZ'])
   return (
     <>
         <h1>GifExpertApp</h1>
+        <ol>
+            { categories.map(category => <li key={category}>{ category }</li>)}
+        </ol>
     </>
   )
 }
