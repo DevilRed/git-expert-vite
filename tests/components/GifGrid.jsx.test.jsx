fc8cde631d6695507914012cbc4b870520de158e
Christian <chrisdaq@gmail.com>
2023-07-17 09:39:55 -0400

    replace useFetchGifs by a mock version

--- tests/components/GifGrid.jsx.test.jsx
+++ tests/components/GifGrid.jsx.test.jsx
@@ -1,12 +1,23 @@
 import { render, screen } from "@testing-library/react";
-import { describe, expect, it } from "vitest";
+import { describe, expect, it, vi } from "vitest";
 import { GifGrid } from "../../src/components/GifGrid";
+import { useFetchGifs } from "../../src/hooks/useFetchGifs";
+
+vi.mock("../../src/hooks/useFetchGifs");
 
 describe('GifGrid', () => {
 	const category = 'One Punch';
+	useFetchGifs.mockReturnValue({
+		data: [],
+		loading: true
+	});
 	it('should show loading on first render while fetching data', () => {
 		render(<GifGrid category={ category } />);
 		expect(screen.getByText('loading...'));
 		expect(screen.getByText(category));
+	});
+
+	it('should display items while fetching images using useFetchGifs', () => {
+		//
 	});
 })
